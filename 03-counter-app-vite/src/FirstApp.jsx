2c09b98b781cb2e445637410bc70f996a66ade41
andres.bravo <andres.bravo161@gmail.com>
2022-07-08 10:26:05 -0300

    Fin seccion 4

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({title, subtitle}) => {
+
+
+  return (
+    <>
+      <h1>{ title }</h1>
+      <p>{subtitle}</p>  
+    </>
+  );
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  subtitle: 'No hay subtitulo'
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso_ReactJS
+## Repositorio para el curso:
+## [React: De cero a experto ( Hooks y MERN )](https://www.udemy.com/course/react-cero-experto/)
